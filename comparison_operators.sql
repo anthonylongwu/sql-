cb4d02b52c46a7801e57b577399d600b16cbd684
Anthony Wu <anthonylongwu19@gmail.com>
2017-03-15 23:11:00 -0700

    comparison operators added

--- comparison_operators.sql
+++ comparison_operators.sql
@@ -1,6 +1,5 @@
-
-#number of rentals in comedy, sports, family
-
+#comparison operators (>, =)
+# <> and != are "not equal to"
 
 SELECT
   c.name as category, count(r.rental_id) as num_rentals
@@ -11,7 +10,18 @@ WHERE
   AND i.film_id = f.film_id 
   AND f.film_id = fc.film_id
   AND fc.category_id = c.category_id
-  AND c.name in ("Comedy", "Sports", "Family")
+  AND c.name != "Comedy"
 GROUP BY
 1
+;
+
+
+# users who have rented at least 3 times
+
+SELECT
+  r.customer_id as customer, count(r.rental_id) as num_rentals
+FROM
+  rental r
+  GROUP BY 1
+HAVING count(r.rental_id) >= 3
 ;
